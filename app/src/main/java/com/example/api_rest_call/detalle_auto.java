de421e8df77aa5066c76e75a5bc2aca1c50f6481
Nicolas Bagini <nicolasbagini@gmail.com>
2020-05-10 11:57:12 -0300

    Va mejor... ya tengo donde esta el problema.
    Lo demas funciona OK.

--- app/src/main/java/com/example/api_rest_call/detalle_auto.java
+++ app/src/main/java/com/example/api_rest_call/detalle_auto.java
@@ -1,19 +1,20 @@
 package com.example.api_rest_call;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelStore;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.BaseAdapter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class detalle_auto extends AppCompatActivity {
 
@@ -22,16 +23,25 @@ public class detalle_auto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_auto);
 
-        Bundle b = getIntent().getExtras();
+        Bundle bundle = getIntent().getExtras();
 
-        String id = b.getString("id");
+        String id = bundle.getString("id");
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://us-central1-be-tp3-a.cloudfunctions.net/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        // Veo que se pasa bien el Id
+        Toast.makeText(detalle_auto.this, id, Toast.LENGTH_LONG).show();
 
-        AutoService autoService = retrofit.create(AutoService.class);
+        Button btn_volver = (Button) findViewById(R.id.btn_volver);
+        Button btn_actualizar = (Button) findViewById(R.id.btn_actualizar);
+        Button btn_eliminar = (Button) findViewById(R.id.btn_eliminar);
+
+        btn_volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                volver();
+            }
+        });
+
+        AutoService autoService = API.getAutoService();
 
         Call<Auto> http_call = autoService.getAuto(id);
 
@@ -39,6 +49,10 @@ public class detalle_auto extends AppCompatActivity {
             @Override
             public void onResponse(Call<Auto> call, Response<Auto> response) {
                 Auto auto = response.body();
+
+                // Veo que el auto esta vacio!!!
+                Toast.makeText(detalle_auto.this, (CharSequence) auto, Toast.LENGTH_LONG).show();
+
 
                 TextView text_id = (TextView) findViewById(R.id.text_id);
                 if (auto!=null){
@@ -53,17 +67,23 @@ public class detalle_auto extends AppCompatActivity {
                     text_modelo.setText(auto.getModelo());
                 }
 
-                /*Toast.makeText(detalle_auto.this,
-                        auto.getMarca(),
-                        Toast.LENGTH_LONG).show();*/
+                Toast.makeText(detalle_auto.this, "Se cargo la informacion del auto.", Toast.LENGTH_LONG);
 
             }
 
             @Override
             public void onFailure(Call<Auto> call, Throwable t) {
-                Toast.makeText(detalle_auto.this, "Hubo un error con la llamada a la API", Toast.LENGTH_LONG);
-
+                Toast.makeText(detalle_auto.this, "Error, fallo la llamada a la API", Toast.LENGTH_LONG);
             }
         });
+
+
+    }
+    public void volver(){
+        Intent intent = new Intent(detalle_auto.this, MainActivity.class);
+
+        Toast.makeText(getApplicationContext(), "Pagina principal.", Toast.LENGTH_LONG);
+
+        startActivity(intent);
     }
 }
